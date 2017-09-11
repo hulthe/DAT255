32a8701c0328f552b85c1fd6af8f8d7a803f6f81
JohnSegerstedt <flexibleterror@hotmail.com>
2017-09-11 08:59:35 +0200

    Added UDPSender variables

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/UDPSender.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/UDPSender.java
@@ -10,5 +10,11 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class UDPSender {
-	
+
+    private final int SERVER_PORT = 8721;
+    private final String ADDRESS = "192.168.43.150";
+
+    private static UDPSender instance;
+
+
 }
