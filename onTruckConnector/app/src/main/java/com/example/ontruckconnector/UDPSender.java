dd6ea406a0f38ee0dad696d6af3f6bd952ac0cb8
JohnSegerstedt <flexibleterror@hotmail.com>
2017-09-11 08:56:46 +0200

    Create UDPSender class
    This class will eventually contain the (client) logic for sending messages over the UDP.

---
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/UDPSender.java
@@ -0,0 +1,14 @@
+package com.example.ontruckconnector;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UDPSender {
+	
+}
