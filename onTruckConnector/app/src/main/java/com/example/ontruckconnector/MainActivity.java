20995cc7ebb1869d4e2f2cf56713740ba669434f
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-09-14 08:29:09 +0200

    Add line to run the TCPChecker in onCreate

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/MainActivity.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.ontruckconnector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new TCPChecker().execute();
     }
 }
