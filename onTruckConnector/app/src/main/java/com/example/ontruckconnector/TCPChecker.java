b5cdbbd6e6dfe3b55a6463bbb1d102e89501aaf4
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-09-14 08:24:36 +0200

    Create empty TCPChecker class

---
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
@@ -0,0 +1,11 @@
+package com.example.ontruckconnector;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by nan on 9/12/17.
+ */
+
+public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
+
+}
