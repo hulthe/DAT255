0f2218aaa8fbf9e018652e383a5dda54d12a8948
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2017-09-14 08:26:14 +0200

    Add check() method

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
@@ -2,10 +2,56 @@ package com.example.ontruckconnector;
 
 import android.os.AsyncTask;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 /**
  * Created by nan on 9/12/17.
  */
 
 public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 
+    private final String IP_ADDRESS;
+    private final int PORT;
+    Socket socket = null;
+
+    private void check() {
+        try {
+            String data = "Hello, How are you?";
+            socket = new Socket(IP_ADDRESS, PORT);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+            //Step 1 send length
+            System.out.println("Length" + data.length());
+            output.writeInt(data.length());
+            //Step 2 send length
+            System.out.println("Writing.......");
+            output.writeBytes(data); // UTF is a string encoding
+
+            //Step 1 read length
+            int nb = input.readInt();
+            byte[] digit = new byte[nb];
+            //Step 2 read byte
+            for (int i = 0; i < nb; i++)
+                digit[i] = input.readByte();
+
+            String st = new String(digit);
+            System.out.println("Received: " + st);
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (socket != null)
+                try {
+                    socket.close();
+                } catch (IOException e) {/*close failed*/}
+        }
+    }
 }
