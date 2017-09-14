c54ae166563a7dfb7b8448188bd57efc0091f25e
JohnSegerstedt <flexibleterror@hotmail.com>
2017-09-14 15:20:22 +0200

    Update TCP client code and fix disconnection bug

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/MainActivity.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/MainActivity.java
@@ -2,17 +2,57 @@ package com.example.ontruckconnector;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import java.io.IOException;
-import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
+	private Button sendButton;
+	private EditText messageText;
+	private TextView connectionText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        new TCPChecker().execute();
+		sendButton = (Button)findViewById(R.id.sendButton);
+		messageText = (EditText)findViewById(R.id.messageText);
+		connectionText = (TextView)findViewById(R.id.connectionText);
+
+
+		sendButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String message = messageText.getText().toString();
+				UDPSender.getInstance().sendMessage(message.getBytes());
+			}
+		});
+
+
+
+		//TODO: Sending MainActivity like this = bad
+        new TCPChecker("192.168.43.150", 8721, this).execute();
     }
+
+
+	public void setConnection(boolean connection){
+		if(connection){
+			updateConnectionText("Connected");}
+		else{
+			updateConnectionText("Disconnected");
+		}
+	}
+
+	private void updateConnectionText(final String text){
+		runOnUiThread(new Runnable(){
+			public void run(){
+				connectionText.setText(text);
+			}
+		});
+	}
+
 }
--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
@@ -1,90 +1,119 @@
 package com.example.ontruckconnector;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import static java.lang.Thread.sleep;
 
-/**
- * Created by nan on 9/12/17.
- */
+
 
 public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 
-    private final String IP_ADDRESS;
-    private final int PORT;
-    Socket socket = null;
 
-    @Override
-    protected TCPChecker doInBackground(String... strings) {
-        run();
-        return null;
-    }
+	private final String IP_ADDRESS;
+	private final int PORT;
+	private final int COOLDOWN = 1000; //this is in ms
+	Socket socket = null;
+	String sentData = "connectiontest";
+	String recievedData = "";
+	boolean connection = false;
+	MainActivity mainActivity;
 
-    public TCPChecker(String ipAddress, int port){
-        IP_ADDRESS = ipAddress;
-        PORT = port;
-    }
+	@Override
+	protected TCPChecker doInBackground(String... strings) {
+		run();
+		return null;
+	}
 
-    public void run() {
-        try{
-            while(true){
-                check();
-                sleep(100);
-            }
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }finally {
-            if(socket != null && !socket.isClosed()){
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	public TCPChecker(String ipAddress, int port, MainActivity mainActivity) {
+		IP_ADDRESS = ipAddress;
+		PORT = port;
+		this.mainActivity = mainActivity;
+	}
 
-    private void check() {
-        try {
-            String data = "Hello, How are you?";
-            socket = new Socket(IP_ADDRESS, PORT);
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+	public void run() {
+		while (true) {
+			try {
+				while (true) {
+					check();
+					sleep(COOLDOWN);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				if (socket != null && !socket.isClosed()) {
+					try {
+						socket.close();
+						socket = null;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
 
-            //Step 1 send length
-            System.out.println("Length" + data.length());
-            output.writeInt(data.length());
-            //Step 2 send length
-            System.out.println("Writing.......");
-            output.writeBytes(data); // UTF is a string encoding
+	private void check() {
+		try {
+			socket = new Socket();
+			socket.connect(new InetSocketAddress(IP_ADDRESS, PORT), COOLDOWN);
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-            //Step 1 read length
-            int nb = input.readInt();
-            byte[] digit = new byte[nb];
-            //Step 2 read byte
-            for (int i = 0; i < nb; i++)
-                digit[i] = input.readByte();
+			//Step 1 send length
+			Log.i("TCP", "Length" + sentData.length());
+			output.writeInt(sentData.length());
+			//Step 2 send length
+			Log.i("TCP", "Writing.......");
+			output.writeBytes(sentData); // UTF is a string encoding
 
-            String st = new String(digit);
-            System.out.println("Received: " + st);
-        } catch (UnknownHostException e) {
-            System.out.println("Sock:" + e.getMessage());
-        } catch (EOFException e) {
-            System.out.println("EOF:" + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("IO:" + e.getMessage());
-        } finally {
-            if (socket != null)
-                try {
-                    socket.close();
-                } catch (IOException e) {/*close failed*/}
-        }
-    }
+			//Step 1 read length
+			int nb = input.readInt();
+			byte[] digit = new byte[nb];
+			//Step 2 read byte
+			for (int i = 0; i < nb; i++)
+				digit[i] = input.readByte();
+
+			recievedData = new String(digit);
+			Log.i("TCP", "Received: " + recievedData);
+		} catch (UnknownHostException e) {
+			Log.i("TCP", "Sock:" + e.getMessage());
+		} catch (EOFException e) {
+			Log.i("TCP", "EOF:" + e.getMessage());
+		} catch (IOException e) {
+			Log.i("TCP", "IO:" + e.getMessage());
+		} catch (NullPointerException e){
+			Log.i("TCP", "NullPointer:" + e.getMessage());
+		} catch (Exception e){
+			Log.i("TCP", "Exception: " + e.getMessage());
+		} finally{
+			if (socket != null)
+				try {
+					socket.close();
+				} catch (IOException e) {/*close failed*/}
+			if (!recievedData.equals("")) {
+				Log.i("TCP", "Connection successful");
+				connection = true;
+			} else {
+				Log.i("TCP", "Connection unsuccessful");
+				connection = false;
+			}
+			this.publishProgress();
+			recievedData = "";
+		}
+	}
+
+	@Override
+	protected void onProgressUpdate(Void... values) {
+		super.onProgressUpdate(values);
+			mainActivity.setConnection(connection);
+	}
 }
