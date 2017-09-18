6e66d1b43ae604c1ee4f3619f25fb59db3d25cde
JohnSegerstedt <flexibleterror@hotmail.com>
2017-09-18 09:26:16 +0200

    Add code comments

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/MainActivity.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/MainActivity.java
@@ -10,9 +10,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+	//All UI-elements. Initializes in onCreate()
 	private Button sendButton;
 	private EditText messageText;
 	private TextView connectionText;
+
 
 
     @Override
@@ -20,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//Initializes the UI-elements
 		sendButton = (Button)findViewById(R.id.sendButton);
 		messageText = (EditText)findViewById(R.id.messageText);
 		connectionText = (TextView)findViewById(R.id.connectionText);
 
+		//Initializes a Holder-object to avoid double coupling between MainActivity and TCPChecker
 		ConnectionTextHolder connectionTextHolder = ConnectionTextHolder.getInstance();
 		connectionTextHolder.setTextView(connectionText);
 
+
+		//Gives the button the ability to send the textfield's text to the UDPChecker
 		sendButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				String message = messageText.getText().toString();
--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/TCPChecker.java
@@ -18,14 +18,13 @@ import static java.lang.Thread.sleep;
 public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 
 
-	private final String IP_ADDRESS;
-	private final int PORT;
-	private final int COOLDOWN = 1000; //this is in ms
+	private final String IP_ADDRESS; 	//the IP_ADDRESS is set in the constructor
+	private final int PORT;				//the port is set in the constructor
+	private final int COOLDOWN = 1000;  //this is in ms and is the delay between pings
 	Socket socket = null;
-	String sentData = "connectiontest";
-	String recievedData = "";
-	boolean connection = false;
-	MainActivity mainActivity;
+	String sentData = "connectiontest"; //"bullshit" data sent to the server
+	String recievedData = "";			//"bullshit" data sent from the server
+	boolean connection = false;			//if there is a TCP connection to the server
 
 	@Override
 	protected TCPChecker doInBackground(String... strings) {
@@ -33,26 +32,36 @@ public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 		return null;
 	}
 
-	public TCPChecker(String ipAddress, int port, MainActivity mainActivity) {
+	//public TCPChecker(String ipAddress, int port, MainActivity mainActivity) {
+	public TCPChecker(String ipAddress, int port) {
 		IP_ADDRESS = ipAddress;
 		PORT = port;
-		this.mainActivity = mainActivity;
 	}
 
 	public void run() {
+		//If we get a connection related exception -> try connecting again
 		while (true) {
+
 			try {
+				//Alternate between the check() code(attempting to connect) and sleeping
 				while (true) {
 					check();
 					sleep(COOLDOWN);
 				}
+
+				//Exception handling
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			} finally {
+
+				//Close socket since it wont have been if an exception was thrown
 				if (socket != null && !socket.isClosed()) {
 					try {
 						socket.close();
 						socket = null;
+
+
+						//Exception handling
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -63,27 +72,31 @@ public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 
 	private void check() {
 		try {
+			//Step 0, create socket and initialize the input stream and the output stream
 			socket = new Socket();
 			socket.connect(new InetSocketAddress(IP_ADDRESS, PORT), COOLDOWN);
 			DataInputStream input = new DataInputStream(socket.getInputStream());
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-			//Step 1 send length
+			//Step 1, send length
 			Log.i("TCP", "Length" + sentData.length());
 			output.writeInt(sentData.length());
-			//Step 2 send length
+
+			//Step 2, send data
 			Log.i("TCP", "Writing.......");
 			output.writeBytes(sentData); // UTF is a string encoding
 
-			//Step 1 read length
+			//Step 3, read length
 			int nb = input.readInt();
 			byte[] digit = new byte[nb];
-			//Step 2 read byte
-			for (int i = 0; i < nb; i++)
-				digit[i] = input.readByte();
 
+			//Step 4, read data
+			for (int i = 0; i < nb; i++){
+				digit[i] = input.readByte();}
 			recievedData = new String(digit);
 			Log.i("TCP", "Received: " + recievedData);
+
+			//Error handling
 		} catch (UnknownHostException e) {
 			Log.i("TCP", "Sock:" + e.getMessage());
 		} catch (EOFException e) {
@@ -95,10 +108,16 @@ public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 		} catch (Exception e){
 			Log.i("TCP", "Exception: " + e.getMessage());
 		} finally{
+
+			//Closes socket
 			if (socket != null)
 				try {
 					socket.close();
-				} catch (IOException e) {/*close failed*/}
+				} catch (IOException e) {
+					Log.i("TCP", "IO:" + e.getMessage());
+				}
+
+			//If statement regarding the received data from the server
 			if (!recievedData.equals("")) {
 				Log.i("TCP", "Connection successful");
 				connection = true;
@@ -106,11 +125,16 @@ public class TCPChecker extends AsyncTask<String, Void, TCPChecker> {
 				Log.i("TCP", "Connection unsuccessful");
 				connection = false;
 			}
+
+			//This gets called after each "tick", calls onProgressUpdate()
 			this.publishProgress();
+
+			//Nulls the received data between each "tick"
 			recievedData = "";
 		}
 	}
 
+	//Gives the connection boolean to the Holder -> possibly toggle the connection text
 	@Override
 	protected void onProgressUpdate(Void... values) {
 		super.onProgressUpdate(values);
