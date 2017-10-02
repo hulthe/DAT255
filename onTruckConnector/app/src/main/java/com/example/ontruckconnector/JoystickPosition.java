aef36266518f312b5f4b0aa99418992d2a8d91bb
JohnSegerstedt <flexibleterror@hotmail.com>
2017-10-02 09:24:34 +0200

    Change trig-logic and log.i new values

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/JoystickPosition.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/JoystickPosition.java
@@ -1,6 +1,7 @@
 package com.example.ontruckconnector;
 
 
+import android.util.Log;
 
 public class JoystickPosition {
 
@@ -13,10 +14,22 @@ public class JoystickPosition {
 
 
 	public void onUpdate(int angle, int strength){
-		byte angleByte = (byte) angle;
-		byte strengthByte = (byte) strength;
-		x = (byte) (Math.cos(angleByte) * strengthByte);
-		y = (byte) (Math.sin(angleByte) * strengthByte);
+		//This converts the circular maximum of the joystick to a square
+		//So you can reach ex: (100,100) instead of (100/sqrt(2), 100/sqrt(2))
+		int newStrength = (int) ((strength * Math.sqrt(20000)) / 100);
+		double dx = Math.cos(angle*Math.PI / 180) * newStrength;
+		double dy = Math.sin(angle*Math.PI / 180) * newStrength;
+
+		if(dx > 100){dx = 100;}
+		else if(dx < -100){dx = -100;}
+		x = (byte)dx;
+
+		if(dy > 100){dy = 100;}
+		else if(dy < -100){dy = -100;}
+		y = (byte)dy;
+
+		//For testing purposes
+		Log.i("Joystick","New x:"+getX()+" | New y:"+ getY());
 	}
 
 
