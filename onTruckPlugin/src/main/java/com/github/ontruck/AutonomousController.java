a5df7e8ee86e7ff115b69bed74a0322775920a26
Lumo <lovisa.landgren@gmail.com>
2017-10-17 17:04:17 +0200

    Add logic for maintaining distance to followed object.

--- onTruckPlugin/src/main/java/com/github/ontruck/AutonomousController.java
+++ onTruckPlugin/src/main/java/com/github/ontruck/AutonomousController.java
@@ -5,6 +5,17 @@ public class AutonomousController extends Thread{
 	private final IDriver driver;
 	private final PlanExecutor planExecutor;
 
+	int newSpeed = 0;
+	//TODO: here we need the distance to the object in front
+	int lastDistance = 0;
+	int currentDistance = 0;
+	int distanceDiff = currentDistance - lastDistance;
+	int goalDistance = 0;
+	// Accepted difference between current distance and goal distance (to prevent constant acceleration, this depends on the accuracy of the distance measurement)
+	int margin = 20;
+	long loopDelay = 800;
+
+
 	public AutonomousController(IDriver driver) {
 		this.driver = driver;
 		this.planExecutor = new PlanExecutor(this.driver);
@@ -13,10 +24,36 @@ public class AutonomousController extends Thread{
 
 	@Override
 	public void run(){
+
 		while (!isInterrupted()) {
-			// get old speed
-			// calculate new speed
-			int newSpeed = 0;
+
+			// delay
+			try {
+				this.sleep(loopDelay);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			//Increase/decrease speed is called only if the distance to the car in front has decreased or increased significally since last interval
+			if (goalDistance < currentDistance + margin && goalDistance > currentDistance - margin) {
+				if (distanceDiff < 2) {
+					continue;
+				}
+			} else if (currentDistance > goalDistance) { //currentDistance is greater than goalDistance
+				if (distanceDiff > 2) { //If difference is significantly positive, we do not need to accelerate more
+					continue;
+				} else {
+					driver.increaseSpeed();
+				}
+			} else { //currentDistance is smaller than goalDistance
+				if (distanceDiff < -2) { //If difference is significantly negative, we do not need to accelerate more
+					continue;
+				} else {
+					driver.decreaseSpeed();
+				}
+			}
+
+			newSpeed = driver.getLastPowerValue();
 
 			// new Instruction
 			Instruction instruction = new Instruction(
@@ -31,6 +68,4 @@ public class AutonomousController extends Thread{
 			planExecutor.newPlan(plan);
 		}
 	}
-
-
 }
