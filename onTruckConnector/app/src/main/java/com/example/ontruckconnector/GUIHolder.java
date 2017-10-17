3c386081de6b5dc21edb3644b8a63828cee9e982
JohnSegerstedt <flexibleterror@hotmail.com>
2017-10-17 15:38:09 +0200

    Add javadoc commenting to GUIHolder and JoystickPosition

--- onTruckConnector/app/src/main/java/com/example/ontruckconnector/GUIHolder.java
+++ onTruckConnector/app/src/main/java/com/example/ontruckconnector/GUIHolder.java
@@ -1,22 +1,39 @@
 package com.example.ontruckconnector;
 
-
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+/**
+ * A singleton class used to avoid double dependencies when trying to change UI elements from different threads.
+ */
 class GUIHolder {
 
+	/**
+	 * The textView which either reads "Connected", if connected, or "Disconnected", if disconnected.
+	 */
 	private TextView connectionText;
+
+	/**
+	 * The togglebutton whcih turns on the ACC.
+	 */
 	private ToggleButton toggleButton;
+
+	/**
+	 * The static singleton object
+	 */
 	private static GUIHolder GUIHolder;
 
 
-
-	//This class is a singleton -> private constructor, only called once
+	/**
+	 * Since this class is a singleton the constructor is private.
+	 */
 	private GUIHolder(){}
 
 
-	//First time this method is called the singleton object is created and stored statically
+	/**
+	 * First time this method is called the singleton object is created and stored statically.
+	 * @return Either a new GUIHolder object or the already created one.
+	 */
 	static GUIHolder getInstance(){
 		if(GUIHolder == null){
 			GUIHolder = new GUIHolder();
@@ -25,17 +42,27 @@ class GUIHolder {
 	}
 
 
-	//The TextView is set here since it can't be set in the constructor
+	/**
+	 * The TextView is set here since it can't be set in the constructor.
+	 * @param textView
+	 */
 	void setTextView(TextView textView) {
 		connectionText = textView;
 	}
 
+	/**
+	 * The ToggleButton is set here since it can't be set in the constructor.
+	 * @param toggleButton
+	 */
 	void setToggleButton(ToggleButton toggleButton){
 		this.toggleButton = toggleButton;
 	}
 
 
-	//This method is called from TCPConnection every "tick"
+	/**
+	 * This method is called from TCPConnection every "tick".
+	 * @param connection If true then the text becomes "Connected", otherwise "Disconnected"
+	 */
 	void setConnection(boolean connection){
 		toggleButton.setChecked(false);
 		if(connection){
@@ -45,6 +72,10 @@ class GUIHolder {
 		}
 	}
 
+	/**
+	 * Private method used to set the text of the textView as long as it isn't null.
+	 * @param text
+	 */
 	//Simply sets TextView's text = the input string
 	private void setConnectionText(final String text) {
 		if(connectionText != null){
